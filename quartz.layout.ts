7f89e17c6bf3264ba031b14b23a9dcb92913c5a9
xX-mabster-Xx <s22d_usolcev@179.ru>
2024-09-24 01:23:39 +0300

    Quartz sync: Sep 24, 2024, 1:23 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'xX-mabster-Xx/public-vault',
+        // from data-repo-id
+        repoId: 'R_kgDOMtev3Q',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOMtev3c4CiucL',
+      }
+    }),
+   ],
   footer: Component.Footer({
     links: {
       "Telegram": "https://t.me/x_mabster_x",
